8cea4c421425733553c61cc5fa2b3a02820a982c
S_KHALIFA <65005134+Psykali@users.noreply.github.com>
2025-06-23 23:25:20 +0200

    new f

--- project_pages/Taxi Fare Analysis.py
+++ project_pages/Taxi Fare Analysis.py
@@ -3,18 +3,20 @@ import json
 import os
 from PIL import Image
 
-PROJECT_DIR = "projects/eiffage_bmi_revit"
+PROJECT_DIR = "projects/taxi_fare_analysis"
 
 with open(os.path.join(PROJECT_DIR, "info.json"), encoding="utf-8") as f:
     data = json.load(f)
 
 st.title(data["title"])
 
-image_path = os.path.join(PROJECT_DIR, data.get("image", "revit_api.png"))
-if os.path.exists(image_path):
-    st.image(Image.open(image_path), use_container_width=True)
+img_path = os.path.join(PROJECT_DIR, data["image"])
+if os.path.exists(img_path):
+    st.image(Image.open(img_path), use_container_width=True)
 
 st.markdown(data["description"], unsafe_allow_html=True)
 
 if data.get("link"):
     st.markdown(f"[📁 GitHub Repo]({data['link']})", unsafe_allow_html=True)
+
+st.markdown("[⬅️ Back to Projects](Projects)")
