3c31225c67483e0ab024765c91c47c95f057fe0f
S_KHALIFA <65005134+Psykali@users.noreply.github.com>
2025-06-25 21:30:11 +0200

    new f

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from pathlib import Path
-# import Home
-# import Projects
+## import Home
+## import Projects
 
 st.set_page_config(page_title="S. Khalifa Portfolio", page_icon="📌", layout="wide")
 
 st.sidebar.title("Navigate")
 page = st.sidebar.radio("", ["Home", "Projects"])
+
 
 if page == "Home":
     import Home
